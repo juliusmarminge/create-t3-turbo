ce40ac7604643c52ede8cd75dc3e15a11c8dd960
Julius Marminge <julius0216@outlook.com>
2023-01-20 11:41:16 +0100

    Initial commit

---
+++ apps/nextjs/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+// src/pages/_app.tsx
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
+import type { AppType } from "next/app";
+
+import { api } from "../utils/api";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
